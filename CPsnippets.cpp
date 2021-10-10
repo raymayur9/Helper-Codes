4e029ec6f4a356841551ee784796bc40d26bb0ac
Mayur Ray <62625019+raymayur9@users.noreply.github.com>
2021-10-10 22:09:38 +0530

    Create CPsnippets.cpp

---
+++ CPsnippets.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#define ll long long int
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        
+    }
+    return 0;
+}
