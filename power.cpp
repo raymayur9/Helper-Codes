2eb93413340a2801fb72b58a04126b13a1bd3d23
Mayur Ray <62625019+raymayur9@users.noreply.github.com>
2021-10-10 22:10:53 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,17 @@
+ll power(ll x, ll y)
+{
+    ll res = 1;
+ 
+    x = x % mod;
+  
+    if (x == 0) return 0;
+ 
+    while (y > 0)
+    {
+        if (y & 1ll)
+            res = (res*x) % mod;
+        y = y>>1ll;
+        x = (x*x) % mod;
+    }
+    return res;
+}
