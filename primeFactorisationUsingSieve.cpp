5e5abb79201abe52d3dc88452c02c8a9d5fd7c7f
Mayur Ray <62625019+raymayur9@users.noreply.github.com>
2021-10-10 22:12:36 +0530

    Create primeFactorisationUsingSieve.cpp

---
+++ primeFactorisationUsingSieve.cpp
@@ -0,0 +1,22 @@
+ll spf[1000001];
+void preprocess()
+{
+    ll n = 1000001;
+    for (ll i = 2; i < n; i++)
+    {
+        spf[i] = i;
+    }
+    for (ll i = 2; i * i < n; i++)
+    {
+        if (spf[i] == i)
+        {
+            for (ll j = i * i; j < n; j += i)
+            {
+                if (spf[j] == j)
+                {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+}
