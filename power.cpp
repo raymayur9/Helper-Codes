4e53bb624114bd987d36238fe086dd3be2277f1e
Mayur Ray <62625019+raymayur9@users.noreply.github.com>
2022-06-06 13:46:11 +0530

    Update power.cpp

--- power.cpp
+++ power.cpp
@@ -1,17 +1,17 @@
 ll power(ll x, ll y)
 {
-    int res = 1;
+    ll res = 1;
 
     x = x % mod;
 
     if (x == 0)
         return 0;
 
-    while (y > 0)
+    while (y)
     {
         if (y & 1ll)
             res = (res * x) % mod;
-        y = y >> 1ll;
+        y >>= 1ll;
         x = (x * x) % mod;
     }
     return res;
